67c421f30873172d084cb9946c283d19d178a1ff
bleecodes <writeto.blee@gmail.com>
2016-09-14 12:52:23 -0400

    ta commit (#3)
    * ta commit

---
+++ src/nyc/c4q/taGroup/Lighter.java
@@ -0,0 +1,12 @@
+package nyc.c4q.taGroup;
+
+import java.lang.reflect.*;
+
+/**
+ * Created by BLEED on 9/13/2016.
+ */
+public class Lighter extends ItemsAtCheckout {
+    public Lighter() {
+        super(Type.NotFood, 1);
+    }
+}
---
+++ src/nyc/c4q/taGroup/Type.java
@@ -0,0 +1,8 @@
+package nyc.c4q.taGroup;
+
+/**
+ * Created by BLEED on 9/13/2016.
+ */
+public enum Type {
+    Food, NotFood
+}
