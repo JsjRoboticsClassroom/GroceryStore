3d74f5b1aad32df40f322230cd7a87c1cf0f83d8
Rusi Li <rusili56@gmail.com>
2016-09-13 21:02:30 -0400

    to dude

--- src/nyc/c4q/group4/HotFood.java
+++ src/nyc/c4q/group4/HotFood.java
@@ -3,4 +3,13 @@ package nyc.c4q.group4;
 import nyc.c4q.instructor.GroceryItem;
 
 public abstract class HotFood extends GroceryItem {
+
+    private final Type tFlavor;
+    private final int iServesHowMany;
+
+    public HotFood (Type pFlavor, int pServesHowMany){
+        this.tFlavor = pFlavor;
+        this.iServesHowMany = pServesHowMany;
+    }
+
 }
