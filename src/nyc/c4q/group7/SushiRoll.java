e27fe008a1dfa65154269b349faf4f5ae6da90ec
Leigh Douglas <leighcdouglas1@gmail.com>
2016-09-16 12:45:13 -0400

    made fish and sauce fields private

--- src/nyc/c4q/group7/SushiRoll.java
+++ src/nyc/c4q/group7/SushiRoll.java
@@ -3,8 +3,8 @@ package nyc.c4q.group7;
 import nyc.c4q.instructor.GroceryItem;
 
 public abstract class SushiRoll extends GroceryItem {
-    Type fish;
-    Sauce sauce;
+    private Type fish;
+    private Sauce sauce;
 
     public SushiRoll(Type fish, Sauce sauce){
         this.fish = fish;
