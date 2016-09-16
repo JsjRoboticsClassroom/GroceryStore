d82f3dab81dfa2fe6fe2e3da1cb39de4be0077e7
leighdouglas <leighcdouglas1@gmail.com>
2016-09-16 13:04:07 -0400

    SushiShop (#5)
    * added sushi shop
    
    * made fish and sauce fields private

--- src/nyc/c4q/group7/SushiRoll.java
+++ src/nyc/c4q/group7/SushiRoll.java
@@ -3,4 +3,12 @@ package nyc.c4q.group7;
 import nyc.c4q.instructor.GroceryItem;
 
 public abstract class SushiRoll extends GroceryItem {
+    private Type fish;
+    private Sauce sauce;
+
+    public SushiRoll(Type fish, Sauce sauce){
+        this.fish = fish;
+        this.sauce = sauce;
+    }
+
 }
