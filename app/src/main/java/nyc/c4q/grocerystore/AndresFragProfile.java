d119d40d762f34429cb3e203fa9badb15b4edde5
cito125 <cito125@gmail.com>
2016-10-08 11:24:03 -0400

    Add Andres Fragment

--- app/src/main/java/nyc/c4q/grocerystore/AndresFragProfile.java
+++ app/src/main/java/nyc/c4q/grocerystore/AndresFragProfile.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AndresFragProfile extends AppCompatActivity {
+    private String First_Fragment_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_andres_frag_profile);
+        setContentView(R.layout.ablank_layout);
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.ablank_layout,new AndresFragment(),First_Fragment_ID)
+                .commit();
     }
 }
--- app/src/main/java/nyc/c4q/grocerystore/AndresFragment.java
+++ app/src/main/java/nyc/c4q/grocerystore/AndresFragment.java
@@ -1,8 +1,19 @@
 package nyc.c4q.grocerystore;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by andresarango on 9/27/16.
  */
 
-public class AndresFragment {
+public class AndresFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.andres_arango,container,false);
+        return view;
+    }
 }
