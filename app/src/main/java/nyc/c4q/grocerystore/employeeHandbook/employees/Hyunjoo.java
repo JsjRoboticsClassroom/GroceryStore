9b55c6330e8c644bad94ac78d86514dbcd7b459d
achowdhury3762 <achowdhury3762@gmail.com>
2016-10-18 01:07:18 -0400

    Reverted Changes Made to other Profile

--- app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/employees/Hyunjoo.java
+++ app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/employees/Hyunjoo.java
@@ -17,6 +17,6 @@ public class Hyunjoo extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.hyunjoo,container,false);
+        return inflater.inflate(R.layout.hyunjoo,container,false);
     }
 }
