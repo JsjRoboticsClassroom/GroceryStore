e60218818b103ffae51b7541b5650b2cca2b0242
Jonathan Johnson <jonathanjohnson@iheartmedia.com>
2016-10-21 13:53:49 -0400

    updates

--- app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/EmployeeHandbookActivity.java
+++ app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/EmployeeHandbookActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import nyc.c4q.grocerystore.R;
 
 public class EmployeeHandbookActivity extends AppCompatActivity{
 
     private RecyclerView mRecyclerView;
+    private TextView mEmployeeCount;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -18,5 +20,8 @@ public class EmployeeHandbookActivity extends AppCompatActivity{
         mRecyclerView = (RecyclerView) findViewById(R.id.employeeList);
         mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
         mRecyclerView.setAdapter(new EmployeeListAdapter());
+        mEmployeeCount = (TextView) findViewById(R.id.select_an_employee);
+        Integer employeeCount = mRecyclerView.getAdapter().getItemCount();
+        mEmployeeCount.setText(getString(R.string.select_an_employee, employeeCount));
     }
 }
