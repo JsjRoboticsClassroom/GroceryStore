b32e59b5aca6d462ceafc5f395e553ca3d61f6e7
tking2005 <tking2005@gmail.com>
2016-10-23 15:24:29 -0400

    added Taryn

--- app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/EmployeeView.java
+++ app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/EmployeeView.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import nyc.c4q.grocerystore.R;
 import nyc.c4q.grocerystore.employeeHandbook.employees.Ashique;
+import nyc.c4q.grocerystore.employeeHandbook.employees.Taryn;
 
 public class EmployeeView extends AppCompatActivity {
     public static final String EMPLOYEE_NAME = "extra.employee.name";
@@ -18,7 +19,17 @@ public class EmployeeView extends AppCompatActivity {
         setContentView(R.layout.employee_view);
         String employee = getIntent().getStringExtra(EMPLOYEE_NAME);
         if(employee != null){
-            // call a function
+            switch (employee){
+                case "Taryn":
+                    showTaryn();
+                    break;
+                case "Ashique":
+                    showAshique();
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 
@@ -26,6 +37,12 @@ public class EmployeeView extends AppCompatActivity {
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.employee_profile,new Ashique());
+        fragmentTransaction.commit();
+    }
+    private void showTaryn(){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.employee_profile,new Taryn());
         fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/employees/Taryn.java
@@ -0,0 +1,23 @@
+package nyc.c4q.grocerystore.employeeHandbook.employees;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nyc.c4q.grocerystore.R;
+
+/**
+ * Created by tarynking on 10/22/16.
+ */
+
+public class Taryn extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.taryn_king,container,false);
+    }
+}
